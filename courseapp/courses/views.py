78fc6fb7683da982f7a721c05529b21573d0a715
chinhhoangcong <chinhhoangcong17@gmail.com>
2024-05-07 23:17:03 +0700

    7/5/2024

--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -1,9 +1,35 @@
-from rest_framework import viewsets,generics
-from .models import Category
-from courses import serializers
+from rest_framework import viewsets, generics, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .models import Category, Course
+from courses import serializers, paginators
 
 
 # Create your views here.
 class CategoryViewSet(viewsets.ViewSet, generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = serializers.CategorySerializer
+
+
+class CourseViewSet(viewsets.ViewSet,generics.ListAPIView):
+    queryset = Course.objects.filter(active=True).all()
+    serializer_class = serializers.CourseSerializer
+    pagination_class = paginators.CoursePaginator
+
+    def get_queryset(self):
+        queries = self.queryset
+
+        q = self.request.query_params.get("q")
+        if q:
+            queries = queries.filter(subject__icontains=q)
+
+        return queries
+
+    @action(methods=['get'], detail=True)
+    def lessons(self, request, pk):
+        lessons = self.get_object().lesson_set.filter(active=True).all()
+
+        return Response(serializers.LessonSerializer(lessons, many=True, context={'request' : request}).data, status=status.HTTP_200_OK)
+
+
